e80bb06b240e8e6209075a06771f333d642c7262
AbhayShaw03 <abhayshaw0301@gmail.com>
2023-12-21 20:18:05 +0530

    setting up database and sanitizing env

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,20 @@
 import 'dotenv/config';
 import express from "express";
+import mongoose from 'mongoose';
+import env from "./util/validateEnv";
 const app = express();
 
-const port=process.env.PORT;
+const port=env.PORT;
 app.get("/",(req,res)=>{
     res.send("Hello World");
 } );
 
-app.listen(port,()=>{
-    console.log(`Server running on ${port}`)
-});
+mongoose.connect(env.MONGO_CONNECTION_STRING)
+.then(()=>{
+    console.log(`Mongoose connected`);
+    app.listen(port,()=>{
+        console.log(`Server running on ${port}`);
+    });
+})
+.catch(console.error);
+
